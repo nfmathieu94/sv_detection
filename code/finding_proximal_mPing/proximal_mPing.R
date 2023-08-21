631b932bf816342a54cfeda76f6d9ed15bc11f47
Nathan Mathieu <nmath020@ucr.edu>
2023-08-21 14:51:55 -0700

    organization

--- code/finding_proximal_mPing/proximal_mPing.R
+++ code/finding_proximal_mPing/proximal_mPing.R
@@ -2,6 +2,7 @@
 library(data.table)
 library(readr)
 library(dplyr)
+library(purrr)
 
 # Make function to append filename during initial reading
 read_plus <- function(flnm) {
@@ -12,12 +13,12 @@ read_plus <- function(flnm) {
 # Use function when reading in data (this should be corrected so that each 
 # end_diff colum will be created for the df before combining
 tbl_with_sources <- 
-    list.files(path = "../Data",
+    list.files(path = "../../data/csv_mPing",
           pattern=".csv",
           full.names = T) %>%
     map_df(~read_plus(.))
 
-tbl_with_sources
+
 
 # Subsetting data frame to get only desired columns
 sub_df <- tbl_with_sources[,c("RIL_Start","end","filename")]
@@ -32,9 +33,16 @@ temp
 # Creating new column with difference in end location value
 diff_df <- 
     temp %>%
-    mutate(end_diff = temp$end - lag(temp$end))
+    mutate(end_diff = end - lag(end))
+#    mutate(end_diff = temp$end - lag(temp$end))
 
-# subsetting for mping that are within 1 kb from one anotheri
+diff_df
+# subsetting for mping that are within 1 kb from one another
 
-df_1kb <- diff_df[diff_df$end_diff < 1000, ]
-View(df_1kb)
+df_6kb <- diff_df[diff_df$end_diff < 6000, ]
+
+# Remove negative values
+df_6kb <- df_6kb %>% filter(end_diff >= 0)
+# Save csv
+write.csv(df_6kb, "../../results/mPing_within_6kb.csv", row.names=FALSE, quote=FALSE)
+
